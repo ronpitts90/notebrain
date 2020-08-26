2157a2e0eb549720a1e1b45efeb7c5d140e639bc
Ronald Pitts <ronpitts81@gmail.com>
2020-08-26 16:06:11 -0400

    adding packages

---
+++ routes/apiroutes.js
@@ -0,0 +1,10 @@
+var path = require("path");
+var fs = require("fs"); 
+
+module.exports = function(app) {
+    app.get("/api/notes`", function(req, res) {
+    //    using res.json pass data from readFile, displaying current notes 
+      });
+
+    //   post will handle getting in new notes
+}
